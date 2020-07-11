109c19f658b697403658cd0af0a257ebd72fcf9e
ritwikvd <ritwikvd@gmail.com>
2020-07-12 03:34:34 +0530

    disabled delete

--- client/src/components/SingleSearchResult.js
+++ client/src/components/SingleSearchResult.js
@@ -1,39 +1,44 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 import SearchContext from "./context/searchContext";
 import Senses from "./Senses";
 
 const SingleSearchResult = () => {
+	const {
+		clearSearch,
+		deleteWord,
+		state: { searchResults }
+	} = useContext(SearchContext);
 
-    const { clearSearch, deleteWord, state: { searchResults } } = useContext(SearchContext);
+	const [confirmDelete, setConfirmDelete] = useState(false);
 
-    const [confirmDelete, setConfirmDelete] = useState(false);
+	const { word, derivatives = [], etymologies, lexicalCategory, phoneticSpelling, senses } = searchResults;
 
-    const { word, derivatives = [], etymologies, lexicalCategory, phoneticSpelling, senses } = searchResults;
+	const confirmAndDelete = () => (confirmDelete ? deleteWord(word) : setConfirmDelete(true));
 
-    const confirmAndDelete = () => confirmDelete ? deleteWord(word) : setConfirmDelete(true);
+	return (
+		<div className="results-container">
+			<h3>{word}</h3>
 
-    return (
-        <div className="results-container">
+			<audio autoPlay controls src={`${process.env.REACT_APP_PROXY}/${word.replace(" ", "_")}.mp3`} />
 
-            <h3>{word}</h3>
+			<p>{phoneticSpelling === "N/A" ? null : phoneticSpelling}</p>
 
-            <audio autoPlay controls src={`${process.env.REACT_APP_PROXY}/${word.replace(" ", "_")}.mp3`} />
+			<p>{lexicalCategory}</p>
 
-            <p>{phoneticSpelling === "N/A" ? null : phoneticSpelling}</p>
+			<p>{derivatives.join("; ")}</p>
 
-            <p>{lexicalCategory}</p>
+			{senses ? <Senses {...{ senses }} /> : null}
 
-            <p>{derivatives.join("; ")}</p>
+			<p>{etymologies.join(",").replace("N/A", "")}</p>
 
-            {senses ? <Senses {...{ senses }} /> : null}
-            
-            <p>{etymologies.join(",").replace("N/A", "")}</p>
-
-            <button className={confirmDelete ? "btn-del-word danger" : "btn-del-word"} onClick={confirmAndDelete}>{confirmDelete ? "Are you sure?" : "Delete Word"}</button>
-            <button className="btn-clear-search" onClick={clearSearch}>Back to all</button>
-
-        </div>
-    );
-}
+			<button disabled className={confirmDelete ? "btn-del-word danger" : "btn-del-word"} onClick={confirmAndDelete}>
+				{confirmDelete ? "Are you sure?" : "Delete Word"}
+			</button>
+			<button className="btn-clear-search" onClick={clearSearch}>
+				Back to all
+			</button>
+		</div>
+	);
+};
 
 export default SingleSearchResult;
